9b53a061c2b534e48aeacfd00b506dc53cd1929a
Nikolay Gudkov <nikola.gudkov@gmail.com>
2025-11-03 13:31:22 +0100

    no sig option

--- src/training.py
+++ src/training.py
@@ -115,11 +115,11 @@ if __name__ == "__main__":
     tr_df = pd.concat(tr_df, axis=0)
     val_df = pd.concat(val_df, axis=0)
 
-    X_tr, log_Y_tr, LLP_tr = data_split(step_size=10, max_samples=10000000, df=tr_df)
-    X_va, log_Y_va, LLP_va = data_split(step_size=10, max_samples=20000000, df=val_df)
+    X_tr, log_Y_tr, LLP_tr = data_split(step_size=10, max_samples=1000, df=tr_df)
+    X_va, log_Y_va, LLP_va = data_split(step_size=10, max_samples=200, df=val_df)
 
-    # X_tr = torch.log(X_tr)
-    # X_va = torch.log(X_va)
+    X_tr = torch.log(X_tr)
+    X_va = torch.log(X_va)
 
     _, best_state, ds_train_mean, ds_train_std = train(X_train = X_tr, Y_train=log_Y_tr, X_val = X_va, Y_val = log_Y_va, LLP_train=LLP_tr, LLP_val=LLP_va, cnf=cnf)
 
